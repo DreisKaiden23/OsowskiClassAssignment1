c6a7f33b25090be9f2d306d4ef7ab929399c047e
DreisKaiden23 <829340@mystma.org>
2021-12-16 09:42:15 -0600

    Added testing file

--- test.py
+++ test.py
@@ -4,15 +4,17 @@
 
 import main;
 import datetime;
+from datetime import date
+today = date.today()
 
-currentYear = int(datetime.datetime.now())
+currentYear = int(today.year)
 year = 2021
 month = 12
 day = 30
 
 def test_code():
-    assert main.ageOneHundred(20) == currentYear + 80, "product3Plus1(2, 2, 2) == 9 failed"
-    assert main.ageOneHundred(59) == currentYear + 41 , "product3Plus1(3, 4, 5) == 61 failed"
+    assert main.ageOneHundred(20) == currentYear + 80, "ageOneHundred(20) == year + 80 failed"
+    assert main.ageOneHundred(59) == currentYear + 41 , "product3Plus1(59) == year + 41 failed"
 
 def test_late():
     assert datetime.datetime.now() < datetime.datetime(year, month, day + 1, 4, 0), "Submitted Late"
